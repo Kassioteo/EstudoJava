336c788601aadda69dbc10886ec3daac33cc00df
Kassioteo <kassioteadoro@gmail.com>
2025-06-16 00:18:37 -0300

    adicionando setter e getter

--- OrientacaoObjetos/src/br/com/galeriaFilme/modelos/Titulo.java
+++ OrientacaoObjetos/src/br/com/galeriaFilme/modelos/Titulo.java
@@ -7,4 +7,52 @@ public class Titulo {
     private String sinopse;
     private int duracao;
     private int classificao;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setClassificao(int classificao) {
+        this.classificao = classificao;
+    }
+
+    public int getClassificao() {
+        return classificao;
+    }
 }
